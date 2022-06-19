cb3bf3e10b5843c94808237222643cfaeee3934b
AinsleySnow <AinsleySnow@outlook.com>
2022-06-19 23:18:43 +0800

    Problem 3 in Chapter 13

--- Chapter 13/Prob3.c
+++ Chapter 13/Prob3.c
@@ -1,33 +1,20 @@
 /*
 *   Solution to Problem 13-3, section b and c.
+*   And more importantly, a complete implementation of avl tree!
 */
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
-#undef max
 
 typedef struct avl_node
 {
     struct avl_node* prev;
     struct avl_node* left;
     struct avl_node* right;
-    int height;
+    int factor;
     int key;
 } avl_node;
-
-int max(int a, int b)
-{
-    return a > b ? a : b;
-}
-
-void refresh_height(avl_node* x)
-{
-    x->height = max(x->left ? x->left->height : 0, 
-                x->right ? x->right->height : 0);
-    if (x->left || x->right)
-        x->height += 1;
-}
 
 void avl_left_rotate(avl_node* x, avl_node** root)
 {
@@ -36,7 +23,7 @@ void avl_left_rotate(avl_node* x, avl_node** root)
 
     if (y->left)
         y->left->prev = x;
-    
+
     y->prev = x->prev;
     if (x->prev)
     {
@@ -51,8 +38,16 @@ void avl_left_rotate(avl_node* x, avl_node** root)
     y->left = x;
     x->prev = y;
 
-    refresh_height(x);
-    refresh_height(y);
+    if (y->factor > 0)
+        x->factor += 1;
+    else
+        x->factor += -(y->factor) + 1;
+    // x->factor += ((-y->factor) & (y->factor >> 31)) + 1;
+    if (x->factor > 0)
+        y->factor += 1;
+    else
+        y->factor += x->factor + 1;
+    // y->factor += (x->factor & (x->factor >> 31)) + 1;
 }
 
 void avl_right_rotate(avl_node* y, avl_node** root)
@@ -62,7 +57,7 @@ void avl_right_rotate(avl_node* y, avl_node** root)
 
     if (x->right)
         x->right->prev = y;
-    
+
     x->prev = y->prev;
     if (y->prev)
     {
@@ -77,44 +72,50 @@ void avl_right_rotate(avl_node* y, avl_node** root)
     x->right = y;
     y->prev = x;
 
-    refresh_height(x);
-    refresh_height(y);
+    if (x->factor < 0)
+        y->factor -= 1;
+    else
+        y->factor -= x->factor + 1;
+    // y->factor -= (~(x->factor >> 31) & x->factor) + 1;
+    if (y->factor > 0)
+        x->factor -= 1;
+    else
+        x->factor += y->factor - 1;
+    // x->factor += (~(y->factor >> 31) & y->factor) - 1;
 }
 
-bool avl_check_balance(avl_node* x)
+void avl_balance(avl_node* x, avl_node** root)
 {
-    int difference = (x->left ? x->left->height : 0) - 
-                     (x->right ? x->right->height : 0);
-    return (difference == 0 || difference == -1 || difference == 1);
-}
-
-void avl_balance(avl_node* unbalance, avl_node** root, int path)
-{
-    int offset = 0, mask = 0b11;
-    while ((mask << offset) & path)
-        offset += 2;
-    path >>= offset - 4;
-
-    switch (path)
+    if (x->factor == 2 && x->left->factor == 1)
+        avl_right_rotate(x, root);
+    else if (x->factor == 2 && x->left->factor == -1)
     {
-        case 0b1111: // case '\':
-        avl_left_rotate(unbalance, root);
-        break;
-
-        case 0b1010: // case '/':
-        avl_right_rotate(unbalance, root);
-        break;
-
-        case 0b1110: // case '<':
-        avl_left_rotate(unbalance->left, root);
-        avl_right_rotate(unbalance, root);
-        break;
-
-        case 0b1011: // case '>':
-        avl_right_rotate(unbalance->right, root);
-        avl_left_rotate(unbalance, root);
-        break;
+        avl_left_rotate(x->left, root);
+        avl_right_rotate(x, root);
     }
+    else if (x->factor == -2 && x->right->factor == -1)
+        avl_left_rotate(x, root);
+    else
+    {
+        avl_right_rotate(x->right, root);
+        avl_left_rotate(x, root);
+    }
+}
+
+void avl_insert_fixup(avl_node* x, avl_node** root)
+{
+    do
+    {
+        if (x == x->prev->left)
+            x->prev->factor++;
+        else
+            x->prev->factor--;
+
+        x = x->prev;
+    } while (x->prev && (x->factor == 1 || x->factor == -1));
+
+    if (x->factor == 2 || x->factor == -2)
+        avl_balance(x, root);
 }
 
 void avl_insert(avl_node** root, int key)
@@ -125,18 +126,15 @@ void avl_insert(avl_node** root, int key)
     do
     {
         trailing = current;
-        
+
         if (current->key > key)
             current = current->left;
         else
             current = current->right;
-    } while(current);
-    
-    current = malloc(sizeof(avl_node));
-    current->height = 0;
+    } while (current);
+
+    current = calloc(1, sizeof(avl_node));
     current->key = key;
-    current->left = NULL;
-    current->right = NULL;
     current->prev = trailing;
 
     if (trailing->key > key)
@@ -144,26 +142,108 @@ void avl_insert(avl_node** root, int key)
     else
         trailing->right = current;
 
-    int path = 0; // '\' 0b11 (3), '/' 0b10 (2).
-    int current_bit = 0;
+    avl_insert_fixup(current, root);
+}
 
-    while (current->prev)
+avl_node* tree_min(avl_node* x)
+{
+    while (x->left)
+        x = x->left;
+    return x;
+}
+
+void transplant(avl_node* u, avl_node* v, avl_node** root)
+{
+    if (u->prev)
     {
-        trailing = current;
-        current = current->prev;
-        if (trailing == current->right)
-            path |= 0b11 << current_bit;
+        if (u == u->prev->left)
+            u->prev->left = v;
         else
-            path |= 0b10 << current_bit;
-        current_bit += 2;
+            u->prev->right = v;
+    }
+    else
+        *root = v;
 
-        refresh_height(current);
-        if (!avl_check_balance(current))
+    if (v)
+        v->prev = u->prev;
+}
+
+void avl_delete_fixup(avl_node* x, avl_node** root)
+{
+    while (x->prev && !x->factor && x->factor != 2 && x->factor != -2)
+    {
+        if (x == x->prev->left)
+            x->prev->factor--;
+        else
+            x->prev->factor++;
+        
+        x = x->prev;
+    }
+
+    if (x->factor == 2 || x->factor == -2)
+        avl_balance(x, root);
+}
+
+void avl_delete(avl_node* z, avl_node** root)
+{
+    avl_node* x = NULL;
+
+    if (!z->left && !z->right)
+    {
+        if (z == z->prev->left)
+            z->prev->factor--;
+        else
+            z->prev->factor++;
+
+        x = z->prev;
+    }
+    else if (!z->right)
+    {
+        transplant(z, z->left, root);
+        z->left->factor = 0;
+        x = z->left;
+    }
+    else
+    {
+        avl_node* succ = tree_min(z->right);
+        if (succ->prev != z)
         {
-            avl_balance(current, root, path);
-            break;
+            transplant(succ, succ->right, root);
+            succ->right = z->right;
+            succ->right->prev = succ;
+
+            succ->prev->factor--;
+            x = succ->prev;
         }
-    }   
+        else
+        {
+            z->right->factor = z->factor - 1;
+            x = z->right;
+        }
+            
+        transplant(z, succ, root);
+        succ->left = z->left;
+        succ->left->prev = succ;
+    }
+
+    avl_delete_fixup(x, root);
+}
+
+avl_node* avl_search(avl_node* head, int key)
+{
+    avl_node* current = head;
+    while (current)
+    {
+        if (current->key == key)
+            return current;
+        
+        if (current->key > key)
+            current = current->left;
+        else
+            current = current->right;
+    }
+
+    return NULL;
 }
 
 void delete_avl_tree(avl_node** root)
@@ -187,7 +267,7 @@ void avl_preorder_walk(avl_node* head, int level)
 {
     if (head)
     {
-        printf("Level %d: %d, height = %d\n", level, head->key, head->height);
+        printf("Level %d: %d, factor = %d\n", level, head->key, head->factor);
         avl_preorder_walk(head->left, level + 1);
         avl_preorder_walk(head->right, level + 1);
     }
@@ -195,16 +275,20 @@ void avl_preorder_walk(avl_node* head, int level)
 
 int main(void)
 {
-    avl_node* root1 = malloc(sizeof(avl_node));
-    root1->height = 0;
-    root1->key = 0;
-    root1->left = NULL;
-    root1->right = NULL;
-    root1->prev = NULL;
+    avl_node* root1 = calloc(1, sizeof(avl_node));
 
-    int data1[10] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+    int data1[10] = { 4, 7, 3, 1, 5, 6, 2, 8, 10, 9 };
     build_avl_tree(&root1, data1, 10);
+
+    printf("How the tree looks like after all these nodes are inserted into it:\n");
     avl_preorder_walk(root1, 0);
+
+    avl_node* node = avl_search(root1, 4);
+    avl_delete(node, &root1);
+    free(node);
+    printf("How the tree looks like after a node was deleted from it:\n");
+    avl_preorder_walk(root1, 0);
+    
     delete_avl_tree(&root1);
 
     return 0;
