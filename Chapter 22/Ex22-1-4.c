66d6a81b9569954791eda756fcaf1d6339741552
AinsleySnow <AinsleySnow@outlook.com>
2022-07-04 06:45:33 +0800

    fixed bug in solution to 22.1.4 The time consumed, however, is not O(V + E). Replacing list with hash table to get desired running time.

--- Chapter 22/Ex22-1-4.c
+++ Chapter 22/Ex22-1-4.c
@@ -2,12 +2,6 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
-typedef struct graph_node
-{
-    int index;
-    adjacency_list adj;
-} graph_node;
-
 typedef struct list_node
 {
     void* content;
@@ -15,37 +9,29 @@ typedef struct list_node
 } list_node;
 
 typedef list_node* adjacency_list;
+
+typedef struct graph_node
+{
+    int index;
+    adjacency_list adj;
+} graph_node;
+
 typedef graph_node* vertex_list;
 
-void append(adjacency_list adj, void* content)
-{
-    list_node* current = adj;
-    list_node* trailing = NULL;
-    while (current)
-    {
-        trailing = current;
-        current = current->next;
-    }
 
-    current = calloc(1, sizeof(list_node));
-    current->content = content;
-    trailing->next = current;
-}
-
-void node_remove(adjacency_list adj, void* content)
+void append(adjacency_list* adj, void* content)
 {
-    list_node* head = adj;
-    list_node* trailing = NULL;
-    while (head)
+    list_node* new_node = calloc(1, sizeof(list_node));
+    new_node->content = content;
+    
+    if (*adj)
     {
-        trailing = head;
-        if (head->content == content)
-        {
-            trailing->next = head->next;
-            free(head);
-            return;
-        }
+        list_node* next = *adj;
+        *adj = new_node;
+        (*adj)->next = next;
     }
+    else
+        *adj = new_node;
 }
 
 bool is_in(adjacency_list adj, void* content)
@@ -61,57 +47,59 @@ bool is_in(adjacency_list adj, void* content)
     return false;
 }
 
-vertex_list get_g_prime(vertex_list original)
+vertex_list get_g_prime(vertex_list original, int n)
 {
-    int n = sizeof(original) / sizeof(graph_node);
+    vertex_list new_graph = calloc(n, sizeof(graph_node));
 
     for (int i = 0; i < n; ++i)
     {
-        list_node* head = original[i].adj;
-        while (head)
+        new_graph[i].index = original[i].index;
+        for (list_node* v = original[i].adj; v; v = v->next)
         {
-            if (head->content != original + i)
+            if (((graph_node*)v->content)->index == i)
+                continue;
+            else if (!is_in(new_graph[i].adj, new_graph + ((graph_node*)v->content)->index))
             {
-                adjacency_list to_be_looked_up = ((graph_node*)head->content)->adj;
-                if (!is_in(to_be_looked_up, original + i))
-                    append(to_be_looked_up, original + i);
+                append(&new_graph[i].adj, new_graph + ((graph_node*)v->content)->index);
+                append(&((new_graph + ((graph_node*)v->content)->index)->adj), new_graph + i);
             }
-            else
-                node_remove(head, original + i);
         }
     }
+
+    return new_graph;
 }
 
 vertex_list build_graph()
 {
     vertex_list vl = calloc(6, sizeof(graph_node));
 
-    vl[0].index = 1;
-    append(vl[0].adj, vl + 1);
-    append(vl[0].adj, vl + 3);
+    vl[0].index = 0;
+    append(&vl[0].adj, vl + 1);
+    append(&vl[0].adj, vl + 3);
 
-    vl[1].index = 2;
-    append(vl[1].adj, vl + 4);
+    vl[1].index = 1;
+    append(&vl[1].adj, vl + 4);
 
-    vl[2].index = 3;
-    append(vl[2].adj, vl + 4);
-    append(vl[2].adj, vl + 5);
+    vl[2].index = 2;
+    append(&vl[2].adj, vl + 4);
+    append(&vl[2].adj, vl + 5);
 
-    vl[3].index = 4;
-    append(vl[3].adj, vl + 2);
+    vl[3].index = 3;
+    append(&vl[3].adj, vl + 2);
+    append(&vl[3].adj, vl);
+    append(&vl[3].adj, vl);
 
-    vl[4].index = 5;
-    append(vl[5].adj, vl + 3);
+    vl[4].index = 4;
+    append(&vl[4].adj, vl + 3);
 
-    vl[5].index = 6;
-    append(vl[5].adj, vl + 5);
+    vl[5].index = 5;
+    append(&vl[5].adj, vl + 5);
 
     return vl;
 }
 
-void print_graph(vertex_list vl)
+void print_graph(vertex_list vl, int n)
 {
-    int n = sizeof(vl) / sizeof(graph_node);
     for (int i = 0; i < n; ++i)
     {
         printf("index = %d\nadjacency list = ", vl[i].index);
@@ -128,8 +116,8 @@ void print_graph(vertex_list vl)
 int main(void)
 {
     vertex_list g = build_graph();
-    get_g_prime(g);
-    print_graph(g);
+    vertex_list new_graph = get_g_prime(g, 6);
+    print_graph(new_graph, 6);
 
     return 0;
 }
