76a981017dc2d5b2d7901817f7bbbc54a93cd113
AinsleySnow <AinsleySnow@outlook.com>
2022-07-04 08:49:28 +0800

    fixed bug in solution to 22.1.6

--- Chapter 22/Ex22-1-6.c
+++ Chapter 22/Ex22-1-6.c
@@ -2,43 +2,22 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
-bool find_universal_sink(int** matrix)
+bool find_universal_sink(int n, int matrix[][n])
 {
     int i = 0, j = 0;
-    int n = sizeof(matrix) / sizeof(int*);
 
-    while (i < n || j < n)
+    while (i < n && j < n)
     {
-        if (i == j)
-        {
-            if (matrix[i][j] == 0)
-                ++i;
-            else
-                ++j;
-        }
-        else
-        {
-            if (matrix[i][j] == 1)
-                ++i;
-            else
-                ++j;
-        }
+        if (matrix[i][j] == 1) ++i;
+        else ++j;
     }
 
-    for (int m = 0; m < n; ++m)
+    for (int k = 0; k < n; ++k)
     {
-        if (m != i)
-        {
-            if (matrix[i][m] != 0)
-                return false;
-            if (matrix[m][i] != 1)
-                return false;
-        }
-        else
-        {
-            if (matrix[i][i] != 0)
-                return false;
-        }
+        if (matrix[i][k] == 1)
+            return false;
+        if (matrix[k][i] == 0 && k != i)
+            return false;
     }
     return true;
 }
@@ -46,7 +25,7 @@ bool find_universal_sink(int** matrix)
 int main(void)
 {
     int matrix1[][6] = {
-        { 0, 1, 0, 1, 0, 0 },
+        { 0, 0, 0, 0, 0, 0 },
         { 1, 0, 0, 1, 0, 0 },
         { 1, 0, 1, 0, 1, 0 },
         { 1, 0, 0, 1, 0, 0 },
@@ -63,8 +42,18 @@ int main(void)
         { 0, 0, 0, 0, 0, 0 }
     };
 
-    printf("%d\n", find_universal_sink(matrix1));
-    printf("%d", find_universal_sink(matrix2));
+    int matrix3[][6] = {
+        { 0, 1, 1, 1, 1, 1 },
+        { 1, 0, 0, 1, 0, 0 },
+        { 0, 0, 1, 0, 1, 1 },
+        { 1, 0, 0, 1, 0, 0 },
+        { 0, 0, 1, 0, 0, 1 },
+        { 0, 0, 0, 0, 1, 0 }
+    };
+
+    printf("%d\n", find_universal_sink(6, matrix1));
+    printf("%d\n", find_universal_sink(6, matrix2));
+    printf("%d", find_universal_sink(6, matrix3));
     
     return 0;
 }
