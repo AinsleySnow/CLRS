bada27ba0f486b2a3cb626c90e47570fc0e69c74
AinsleySnow <AinsleySnow@outlook.com>
2022-07-04 19:05:01 +0800

    fixed bugs in solution to 22.1.5

--- Chapter 22/Ex22-1-5.c
+++ Chapter 22/Ex22-1-5.c
@@ -1,11 +1,11 @@
-#include "List/List.h"
+#include "List\List.h"
 #include <stdio.h>
 #include <stdlib.h>
 
 #define get_content(x)      ((graph_node*)((x)->content))
 #define mark_color(x, y)    (x) |= ((y) << 30)
 #define get_color(x)        (((x) >> 30) & 0b11)
-#define isConnected(x)      ((x) & 1)
+#define is_connected(x)      ((x) & 1)
 #define mark_pi(x, y)       (x) |= (((y) & 0x1fffffff) << 1)
 #define get_pi(x)           (((x) >> 1) & 0x1fffffff)
 
@@ -21,6 +21,7 @@ typedef list* adjacency_list;
 typedef struct graph_node
 {
     color c;
+    int index;
     struct graph_node* pi;
     adjacency_list adj;
 } graph_node;
@@ -28,10 +29,14 @@ typedef struct graph_node
 typedef graph_node* pnode;
 typedef graph_node* vertex_list;
 
-vertex_list find_g_square_list(vertex_list g)
+vertex_list find_g_square_list(vertex_list g, int n)
 {
-    int n = sizeof(g) / sizeof(graph_node);
     vertex_list g_square = calloc(n, sizeof(graph_node));
+    for (int i = 0; i < n; ++i)
+    {
+        g_square[i].index = i;
+        g_square[i].adj = calloc(1, sizeof(list));
+    }
 
     for (int i = 0; i < n; ++i)
     {
@@ -43,7 +48,7 @@ vertex_list find_g_square_list(vertex_list g)
             {
                 get_content(current)->c = gray;
                 get_content(current)->pi = g + i;
-                list_append(g_square[i].adj, current);
+                list_append(g_square[i].adj, g_square + get_content(current)->index);
                 current = current->next;
             }
         }
@@ -56,8 +61,9 @@ vertex_list find_g_square_list(vertex_list g)
             list_node* current = g[i].adj->head;
             while (current)
             {
-                list_append(g_square[i].adj, current);
-                list_append((g_square + (g[i].pi - g))->adj, current);
+                list_append(g_square[i].adj, g_square + get_content(current)->index);
+                list_append((g_square + (g[i].pi - g))->adj, g_square + get_content(current)->index);
+                current = current->next;
             }
         }
     }
@@ -68,6 +74,12 @@ vertex_list find_g_square_list(vertex_list g)
 vertex_list bulid_list()
 {
     vertex_list vl = calloc(6, sizeof(graph_node));
+    for (int i = 0; i < 6; ++i)
+    {
+        vl[i].index = i;
+        vl[i].c = white;
+        vl[i].adj = calloc(1, sizeof(list));
+    }
 
     list_append(vl[0].adj, vl + 1);
     list_append(vl[0].adj, vl + 3);
@@ -86,29 +98,28 @@ vertex_list bulid_list()
     return vl;
 }
 
-void print_graph(vertex_list vl)
+void print_graph(vertex_list vl, int n)
 {
-    int n = sizeof(vl) / sizeof(graph_node);
     for (int i = 0; i < n; ++i)
     {
         printf("index = %d\nadjacency list = ", vl[i].index);
-        list_node* head = vl[i].adj;
-        while (head)
+        list_node* current = vl[i].adj->head;
+        while (current)
         {
-            printf("%d ", ((graph_node*)head->content)->index);
-            head = head->next;
+            printf("%d ", ((graph_node*)current->content)->index);
+            current = current->next;
         }
         printf("\n");
     }
 }
 
-int** find_g_square_matrix(int** g)
+int** find_g_square_matrix(int** g, int n)
 {
-    int n = sizeof(g) / sizeof(int*);
     int** g_square = calloc(n, sizeof(int*));
     for (int i = 0; i < n; ++i)
         g_square[i] = calloc(n, sizeof(int));
 
+    // 0: white 1: black 2: gray
     for (int i = 0; i < n; ++i)
     {
         if (!get_color(g[i][0]))
@@ -116,7 +127,7 @@ int** find_g_square_matrix(int** g)
             mark_color(g[i][0], 1);
             for (int j = 0; j < n; ++j)
             {
-                if (isConnected(g[i][j]))
+                if (is_connected(g[i][j]))
                 {
                     mark_color(g[j][0], 2);
                     mark_pi(g[j][0], i);
@@ -133,7 +144,7 @@ int** find_g_square_matrix(int** g)
             int pi = get_pi(g[i][0]);
             for (int j = 0; j < n; ++j)
             {
-                if (isConnected(g[i][j]))
+                if (is_connected(g[i][j]))
                 {
                     g_square[i][j] = 1;
                     g_square[pi][j] = 1;
@@ -159,10 +170,9 @@ int** bulid_matrix()
     matrix[2][4] = 1;
     matrix[2][5] = 1;
 
-    matrix[3][1] = 1;
+    matrix[3][2] = 1;
 
-    matrix[4][3] = 1;
-
+    matrix[5][3] = 1;
     matrix[5][5] = 1;
 
     return matrix;
@@ -171,8 +181,8 @@ int** bulid_matrix()
 int main(void)
 {
     vertex_list vl = bulid_list();
-    vertex_list vl_square = find_g_square_list(vl);
-    print_graph(vl_square);
+    vertex_list vl_square = find_g_square_list(vl, 6);
+    print_graph(vl_square, 6);
 
     for (int i = 0; i < 6; ++i)
     {
@@ -182,10 +192,20 @@ int main(void)
     free(vl);
     free(vl_square);
 
+
+    printf("\n\n");
+
+
     int** matrix = bulid_matrix();
-    int** matrix_square = find_g_square_matrix(matrix);
+    int** matrix_square = find_g_square_matrix(matrix, 6);
+    
+    printf(" \\ | ");
+    for (int i = 0; i < 6; ++i)
+        printf("%d ", i);
+    printf("\n---+-------------\n");
     for (int i = 0; i < 6; ++i)
     {
+        printf(" %d | ", i);
         for (int j = 0; j < 6; ++j)
             printf("%d ", matrix_square[i][j]);
         printf("\n");
