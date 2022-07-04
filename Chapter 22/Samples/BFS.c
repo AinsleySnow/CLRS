3cbb913f97a19fde0ea050ad77358a1749230c8c
AinsleySnow <AinsleySnow@outlook.com>
2022-07-04 19:30:21 +0800

    fixed bugs in implementation of BFS

--- Chapter 22/Samples/BFS.c
+++ Chapter 22/Samples/BFS.c
@@ -8,34 +8,28 @@
 typedef enum color
 {
     white,
-    black,
-    gray
+    gray,
+    black
 } color;
 
 typedef struct graph_node
 {
     int d;
     color c;
-    pnode pi;
-    adjacency_list adj;
+    int adj_len;
+    struct graph_node* pi;
+    struct graph_node** adj;
 } graph_node;
 
 typedef graph_node* pnode;
 typedef graph_node* vertex_list;
 typedef graph_node** adjacency_list;
 
-void bfs(vertex_list graph, pnode source)
+void bfs(vertex_list graph, pnode source, int n)
 {
-    int n = sizeof(graph) / sizeof(graph_node);
     for (int i = 0; i < n; ++i)
-    {
         if (graph + i != source)
-        {
-            graph[i].c = white;
             graph[i].d = INFINITE;
-            graph[i].pi = NULL;
-        }
-    }
 
     source->c = gray;
     source->d = 0;
@@ -46,7 +40,7 @@ void bfs(vertex_list graph, pnode source)
     while (!is_empty(&q))
     {
         pnode u = dequeue(&q);
-        int adj_len = sizeof(u->adj) / sizeof(pnode);
+        int adj_len = u->adj_len;
         for (int i = 0; i < adj_len; ++i)
         {
             pnode v = u->adj[i];
@@ -56,7 +50,7 @@ void bfs(vertex_list graph, pnode source)
                 v->d = u->d + 1;
                 v->pi = u;
                 enqueue(&q, v);
-            }    
+            }   
         }
         u->c = black;
     }
@@ -66,60 +60,66 @@ vertex_list build_graph()
 {
     vertex_list g = calloc(8, sizeof(graph_node));
     g[0].adj = calloc(2, sizeof(pnode));
-    g[0].adj[0] = &g[1];
-    g[0].adj[1] = &g[4];
+    g[0].adj_len = 2;
+    g[0].adj[0] = g + 1;
+    g[0].adj[1] = g + 4;
 
     g[1].adj = calloc(2, sizeof(pnode));
-    g[1].adj[0] = &g[0];
-    g[1].adj[1] = &g[5];
+    g[1].adj_len = 2;
+    g[1].adj[0] = g;
+    g[1].adj[1] = g + 5;
 
     g[2].adj = calloc(3, sizeof(pnode));
-    g[2].adj[0] = &g[3];
-    g[2].adj[1] = &g[5];
-    g[2].adj[2] = &g[6];
+    g[2].adj_len = 3;
+    g[2].adj[0] = g + 3;
+    g[2].adj[1] = g + 5;
+    g[2].adj[2] = g + 6;
 
     g[3].adj = calloc(3, sizeof(pnode));
-    g[3].adj[0] = &g[2];
-    g[3].adj[1] = &g[6];
-    g[3].adj[2] = &g[7];
+    g[3].adj_len = 3;
+    g[3].adj[0] = g + 2;
+    g[3].adj[1] = g + 6;
+    g[3].adj[2] = g + 7;
 
     g[4].adj = calloc(1, sizeof(pnode));
-    g[4].adj[0] = &g[0];
+    g[4].adj_len = 1;
+    g[4].adj[0] = g;
 
     g[5].adj = calloc(3, sizeof(pnode));
-    g[5].adj[0] = &g[1];
-    g[5].adj[1] = &g[2];
-    g[5].adj[2] = &g[6];
+    g[5].adj_len = 3;
+    g[5].adj[0] = g + 1;
+    g[5].adj[1] = g + 2;
+    g[5].adj[2] = g + 6;
 
     g[6].adj = calloc(4, sizeof(pnode));
-    g[6].adj[0] = &g[2];
-    g[6].adj[1] = &g[3];
-    g[6].adj[2] = &g[5];
-    g[6].adj[3] = &g[7];
+    g[6].adj_len = 4;
+    g[6].adj[0] = g + 2;
+    g[6].adj[1] = g + 3;
+    g[6].adj[2] = g + 5;
+    g[6].adj[3] = g + 7;
 
     g[7].adj = calloc(2, sizeof(pnode));
-    g[7].adj[0] = &g[3];
-    g[7].adj[1] = &g[6];
+    g[7].adj_len = 2;
+    g[7].adj[0] = g + 3;
+    g[7].adj[1] = g + 6;
 
     return g;
 }
 
-void print_graph(vertex_list vl)
+void print_graph(vertex_list vl, int n)
 {
-    int n = sizeof(vl) / sizeof(graph_node);
     for (int i = 0; i < n; ++i)
     {
-        printf("%dth vertex:\n");
+        printf("vertex %d:\n", i);
         printf("distence = %d\n", vl[i].d);
         printf("color = %d\n", vl[i].c);
-        printf("pi = %d\n", (int)(vl[i].pi - vl));
+        printf("pi = %d\n", vl[i].pi ? (int)(vl[i].pi - vl) : -1);
         printf("\n");
     }
 }
 
-void delete_graph(vertex_list vl)
+void delete_graph(vertex_list vl, int n)
 {
-    int n = sizeof(vl) / sizeof(graph_node);
     for (int i = 0; i < n; ++i)
         free(vl[i].adj);
     free(vl);
@@ -128,9 +128,9 @@ void delete_graph(vertex_list vl)
 int main(void)
 {
     vertex_list vl = build_graph();
-    bfs(vl, vl + 1);
-    print_graph(vl);
+    bfs(vl, vl + 1, 8);
+    print_graph(vl, 8);
 
-    delete_graph(vl);
+    delete_graph(vl, 8);
     return 0;
 }
