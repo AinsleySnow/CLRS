e490541cc8e713f1f5533ad575a77945948ca3ee
AinsleySnow <AinsleySnow@outlook.com>
2022-07-05 10:24:32 +0800

    added solution to 22.2.8

--- Chapter 22/Ex22-2-8.c
+++ Chapter 22/Ex22-2-8.c
@@ -4,45 +4,50 @@
 
 typedef struct tree_node
 {
+    int chld_num;
+    int d;
     struct tree_node* prev;
     struct tree_node** children;
 } tree_node;
 
 int get_diameter(tree_node* head)
 {
-    queue q;
+    queue q = { NULL, NULL };
     enqueue(&q, head);
     tree_node* u = NULL;
     while (!is_empty(&q))
     {
         u = dequeue(&q);
-        for (int i = 0; i < sizeof(u->children) / sizeof(tree_node*); ++i)
+        for (int i = 0; i < u->chld_num; ++i)
+        {
+            u->children[i]->d = u->d + 1;
             enqueue(&q, u->children[i]);
+        }     
     }
 
-    int d = 0;
-    while (u)
-    {
-        u = u->prev;
-        ++d;
-    }
-    return d;
+    return u->d + 1;
 }
 
 tree_node* build_tree()
 {
     tree_node* head = calloc(1, sizeof(tree_node));
+    head->chld_num = 2;
     head->children = calloc(2, sizeof(tree_node*));
 
     head->children[0] = calloc(1, sizeof(tree_node));
     head->children[0]->prev = head;
+
     head->children[0]->children = calloc(2, sizeof(tree_node*));
+    head->children[0]->chld_num = 2;
     head->children[0]->children[0] = calloc(1, sizeof(tree_node));
     head->children[0]->children[0]->prev = head->children[0];
     head->children[0]->children[1] = calloc(1, sizeof(tree_node));
     head->children[0]->children[1]->prev = head->children[0];
-    *(head->children[0]->children[1]->children) = calloc(1, sizeof(tree_node));
-    (*(head->children[0]->children[1]->children))->prev = head->children[0]->children[1];
+    
+    head->children[0]->children[1]->children = calloc(1, sizeof(tree_node*));
+    head->children[0]->children[1]->chld_num = 1;
+    head->children[0]->children[1]->children[0] = calloc(1, sizeof(tree_node));
+    head->children[0]->children[1]->children[0]->prev = head->children[0]->children[1];
 
     head->children[1] = calloc(1, sizeof(tree_node));
 
@@ -52,7 +57,7 @@ tree_node* build_tree()
 void free_tree(tree_node* head)
 {
     if (head->children)
-        for (int i = 0; i < sizeof(head->children) / sizeof(tree_node*); ++i)
+        for (int i = 0; i < head->chld_num; ++i)
             free_tree(head->children[i]);
     
     free(head);
