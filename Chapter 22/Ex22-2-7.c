7e7b9faf7f0a6af512d9fb20f790ecca8fea04f9
AinsleySnow <AinsleySnow@outlook.com>
2022-07-05 08:28:19 +0800

    added solution to 22.2.7

--- Chapter 22/Ex22-2-7.c
+++ Chapter 22/Ex22-2-7.c
@@ -21,6 +21,7 @@ typedef struct wrestler
     int d;
     color cr;
     category cy;
+    int adj_len;
     struct wrestler** adj;
 } wrestler;
 
@@ -28,18 +29,17 @@ typedef wrestler* vertex_list;
 typedef wrestler* pwrestler;
 typedef wrestler** adjacency_list;
 
-void mark_category(vertex_list vl)
+void mark_category(vertex_list vl, int n)
 {
-    int n = sizeof(vl) / sizeof(wrestler);
-    queue q;
+    queue q = { NULL, NULL };
     enqueue(&q, vl);
     vl[0].cr = black;
     vl[0].d = 0;
 
-    while (is_empty(&q))
+    while (!is_empty(&q))
     {
         pwrestler pw = dequeue(&q);
-        int adj_len = sizeof(pw->adj) / sizeof(pwrestler);
+        int adj_len = pw->adj_len;
 
         for (int i = 0; i < adj_len; ++i)
         {
@@ -50,6 +50,7 @@ void mark_category(vertex_list vl)
                 v->d = pw->d + 1;
             }
         }
+        pw->cr = black;
     }
 
     for (int i = 0; i < n; ++i)
@@ -61,71 +62,113 @@ void mark_category(vertex_list vl)
     }
 }
 
-bool check(vertex_list vl)
+bool check(vertex_list vl, int n)
 {
-    int n = sizeof(vl) / sizeof(wrestler);
     for (int i = 0; i < n; ++i)
     {
-        int adj_len = sizeof(vl[i].adj) / sizeof(pwrestler);
+        int adj_len = vl[i].adj_len;
         for (int j = 0; j < adj_len; ++j)
-            if (vl[i].adj[j]->cy == vl[i].cy)
+            if ((vl[i].adj[j])->cy == vl[i].cy)
                 return false;
     }
 
     return true;
 }
 
-vertex_list build_graph()
+vertex_list build_graph1()
 {
     vertex_list vl = calloc(8, sizeof(wrestler));
 
     vl[0].adj = calloc(2, sizeof(pwrestler));
+    vl[0].adj_len = 2;
     vl[0].adj[0] = vl + 1;
     vl[0].adj[1] = vl + 3;
 
     vl[1].adj = calloc(3, sizeof(pwrestler));
+    vl[1].adj_len = 3;
     vl[1].adj[0] = vl;
     vl[1].adj[1] = vl + 2;
     vl[1].adj[2] = vl + 3;
 
     vl[2].adj = calloc(3, sizeof(pwrestler));
+    vl[2].adj_len = 3;
     vl[2].adj[0] = vl + 1;
     vl[2].adj[1] = vl + 4;
     vl[2].adj[2] = vl + 5;
 
     vl[3].adj = calloc(3, sizeof(pwrestler));
+    vl[3].adj_len = 3;
     vl[3].adj[0] = vl;
     vl[3].adj[1] = vl + 1;
     vl[3].adj[2] = vl + 4;
 
     vl[4].adj = calloc(3, sizeof(pwrestler));
+    vl[4].adj_len = 3;
     vl[4].adj[0] = vl + 2;
     vl[4].adj[1] = vl + 3;
     vl[4].adj[2] = vl + 5;
 
     vl[5].adj = calloc(4, sizeof(pwrestler));
+    vl[5].adj_len = 4;
     vl[5].adj[0] = vl + 2;
     vl[5].adj[1] = vl + 4;
     vl[5].adj[2] = vl + 6;
     vl[5].adj[3] = vl + 7;
 
     vl[6].adj = calloc(1, sizeof(pwrestler));
+    vl[6].adj_len = 1;
     vl[6].adj[0] = vl + 5;
 
     vl[7].adj = calloc(1, sizeof(pwrestler));
+    vl[7].adj_len = 1;
     vl[7].adj[0] = vl + 5;
+
+    return vl;
+}
+
+vertex_list build_graph2()
+{
+    vertex_list vl = calloc(4, sizeof(wrestler));
+
+    vl[0].adj = calloc(2, sizeof(pwrestler));
+    vl[0].adj_len = 2;
+    vl[0].adj[0] = vl + 1;
+    vl[0].adj[1] = vl + 2;
+
+    vl[1].adj = calloc(2, sizeof(pwrestler));
+    vl[1].adj_len = 2;
+    vl[1].adj[0] = vl;
+    vl[1].adj[1] = vl + 3;
+
+    vl[2].adj = calloc(2, sizeof(pwrestler));
+    vl[2].adj_len = 2;
+    vl[2].adj[0] = vl;
+    vl[2].adj[1] = vl + 3;
+
+    vl[3].adj = calloc(2, sizeof(pwrestler));
+    vl[3].adj_len = 2;
+    vl[3].adj[0] = vl + 1;
+    vl[3].adj[1] = vl + 2;
 
     return vl;
 }
 
 int main(void)
 {
-    vertex_list g = build_graph();
-    mark_category(g);
-    printf("%d", check(g));
+    vertex_list g1 = build_graph1();
+    mark_category(g1, 8);
+    printf("%d\n", check(g1, 8));
+
+    vertex_list g2 = build_graph2();
+    mark_category(g2, 4);
+    printf("%d\n", check(g2, 4));
 
     for (int i = 0; i < 8; ++i)
-        free(g[i].adj);
-    free(g);
+        free(g1[i].adj);
+    free(g1);
+    for (int i = 0; i < 4; ++i)
+        free(g2[i].adj);
+    free(g2);
+
     return 0;
 }
