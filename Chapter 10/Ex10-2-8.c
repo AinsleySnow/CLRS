c5fb83c06d5c583fb7a21794efaf8531d4a1638a
AinsleySnow <AinsleySnow@outlook.com>
2022-07-08 21:10:51 +0800

    fixed bugs in solution to 10.2.8 I must have been dreaming when I was writing the code.

--- Chapter 10/Ex10-2-8.c
+++ Chapter 10/Ex10-2-8.c
@@ -1,73 +1,119 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
 
 typedef struct node
 {
-    long np;
+    uint64_t np;
     int v;
-}node;
+} node;
 
-node search(node* head, int key)
+node* search(node* head, int key)
 {
-    node* next = head->np;
-    node* current = NULL;
+    node* next = NULL;
+    node* prev = NULL;
+    node* current = head;
 
-    do
+    while (current)
     {
-        current = next;
         if (current->v == key)
             return current;
-        next = (node*)((long)next ^ head->np);
-    } while (current);
+        next = (node*)((uint64_t)prev ^ (uint64_t)current->np);
+        prev = current;
+        current = next;
+    }
 
-    return { 0, 0 };
+    return NULL;
 }
 
 void insert(node* head, int key)
 {
-    node* x = malloc(sizeof(node));
     node* current = head;
-    node* trailing = NULL;
-
-    node* prev = 0;
-    node* temp = NULL;
-    while (temp = (node*)((long)(current->np) ^ prev))
+    node* prev = NULL;
+    node* next = NULL;
+    while ((next = (node*)((uint64_t)(current->np) ^ (uint64_t)(prev))))
     {
         prev = current;
-        current = temp;
+        current = next;
     }
 
-    x->np = prev;
-    x->v = key;
-    current->np ^= (long)x;
+    node* new_node = calloc(1, sizeof(node));
+    new_node->np = (uint64_t)current;
+    new_node->v = key;
+    current->np ^= (uint64_t)new_node;
 }
 
 void delete(node* head, node* x)
 {
-    node* current = (node*)head;
-    node* next = (node*)head->np;
+    node* next = NULL;
+    node* prev = NULL;
+    node* current = head;
+
+    while (current)
+    {
+        if (current == x)
+            goto find;
+        next = (node*)((uint64_t)prev ^ (uint64_t)current->np);
+        prev = current;
+        current = next;
+    }
+    return;
+
+find:
+    next = (node*)((uint64_t)prev ^ (uint64_t)current->np);
+    next->np = next->np ^ (uint64_t)current ^ (uint64_t)prev;
+    prev->np = prev->np ^ (uint64_t)current ^ (uint64_t)next;
+    free(current);
+}
+
+void free_list(node* head)
+{
+    node* current = head;
     node* prev = NULL;
     node* temp = NULL;
 
     while (current)
     {
-        if (current == x)
-        {
-            prev = (node*)(current->np ^ (long)next);
-            current = next;
-            next = (node*)((long)next ^ current->np);
-            prev->np = prev->np ^ (long)current;
-            next->np = next->np ^ (long)current;
-            next->np = next->np ^ (long)prev;
-            return;
-        }
-        temp = (node*)((long)(current->np) ^ prev);
-        prev = current
-            current = temp;
+        temp = (node*)((uint64_t)(current->np) ^ (uint64_t)prev);
+        prev = current;
+        free(current);
+
+        current = temp;
     }
 }
 
 int main(void)
 {
+    node* head = calloc(1, sizeof(node));
+    head->v = 4;
 
+    insert(head, 1);
+    insert(head, 2);
+    insert(head, 1000);
+    insert(head, 387);
+    insert(head, 2387498);
+
+    node* ans_node = search(head, 9);
+    if (ans_node)
+    {
+        printf("This line should not have been printed.\n");
+        return 0;
+    }
+
+    ans_node = search(head, 1);
+    printf("%d\n", ans_node->v);
+    ans_node = search(head, 1000);
+    printf("%d\n", ans_node->v);
+
+    delete(head, ans_node);
+    ans_node = search(head, 1000);
+    if (ans_node)
+    {
+        printf("This line should not have been printed.\n");
+        return 0;
+    }
+
+    free_list(head);
+
+    return 0;
 }
